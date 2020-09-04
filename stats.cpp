cf4a8ce6aee91e5c4ae6d8c669cfc9be057911ef
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-09-04 09:28:37 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -25,7 +25,7 @@ namespace Statistics {
         statsObj.average = accumulate(floatVectorWithoutNAN.begin(), floatVectorWithoutNAN.end(), 0.0) / floatVectorWithoutNAN.size();
         statsObj.max = *max_element(floatVectorWithoutNAN.begin(), floatVectorWithoutNAN.end());
         statsObj.min = *min_element(floatVectorWithoutNAN.begin(), floatVectorWithoutNAN.end());
-        std::cout<<statsObj.average<<" "statsObj.max<<" "statsObj.min<<std::endl;
+        std::cout<<statsObj.average<<" "<< tatsObj.max<<" "<<statsObj.min<<std::endl;
         
         return statsObj;
     }
